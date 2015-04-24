8e3b690f03d5e09b1433ff300d1d86039bdd3ebe
Kevin Cain <kevinmcain@hotmail.com>
2015-04-24 14:59:57 -0700

    fixed bug "Cannot GET/".

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 //serve static images for the app from the 'images' directory
-app.use('/images', express.static('./images'));
+app.use('/images', express.static(__dirname + '/images'));
 //serve static images for the app from the 'application_pages' directory in the app dir
-app.use(express.static('./application_pages/'));
+app.use(express.static(__dirname + '/application_pages/'));
 app.listen(80);
