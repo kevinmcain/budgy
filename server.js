549ad28aa0e46a142e1a507904adc498ee49fd51
Kevin Cain <kevinmcain@hotmail.com>
2015-05-02 11:43:24 -0700

    testing mongoose

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
 //serve static images for the app from the 'images' directory
 app.use('/images', express.static(__dirname + '/images'));
 //serve static images for the app from the 'application_pages' directory in the app dir
 app.use(express.static(__dirname + '/application_pages/'));
+
+if('development' == app.get('env')) {
+	//app.use(express.errorHandler());
+	mongoose.connect('mongodb://127.0.0.1/budgy');
+}
+
+app.get('/', function(req, res) {
+	res.send('ok');
+});
 app.listen(80);
