2c6396af79f252fcd11e07d6a2f4ea8c1def2516
kevinmcain <kevinmcain@hotmail.com>
2015-05-02 15:24:36 -0700

    Implemented querying for a users envelopes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var db = mongoose.connection;
 
 //serve static images for the app from the 'images' directory
 app.use('/images', express.static(__dirname + '/images'));
@@ -8,11 +9,41 @@ app.use('/images', express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/application_pages/'));
 
 if('development' == app.get('env')) {
-	//app.use(express.errorHandler());
+	
 	mongoose.connect('mongodb://127.0.0.1/budgy');
 }
 
-app.get('/', function(req, res) {
-	res.send('ok');
+var envelopeSchema = mongoose.Schema({
+	_id: Object,
+	bid: String,
+	cid: String,
+	category: String,
+	amount: String,
+	spent: String,
+	balance: String
 });
+
+// specify modelName, schemaObject, collectionName
+var envelopeModel = 
+	mongoose.model('envelopeModel', envelopeSchema, 'envelope');
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function (callback) {
+  	console.log('opened mongo connection');
+});
+
+app.get('/envelopes/:budgetId', function (req, res) {
+	
+	var budgetId = req.params.budgetId;
+	
+	console.log('requested bid: %s', budgetId);
+	
+	// query for envelopes given the budget id parameter
+	envelopeModel.find({bid: budgetId}, function(err, envelopes) {
+		res.send(envelopes);
+	});
+	
+});
+
 app.listen(80);
