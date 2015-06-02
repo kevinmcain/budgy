5933367fdcaa6ede8f4a228b904b0518f8ffe872
kevinmcain <kevinmcain@hotmail.com>
2015-06-01 23:55:46 -0700

    added logic to link budget documents to user documents

--- UserAuthentication/mongoScript.user.js
+++ UserAuthentication/mongoScript.user.js
@@ -1,3 +1,7 @@
+var emailSravani = 'sravani@seattleu.edu'
+var emailIshwant = 'kauri@seattleu.edu'
+var emailKevin = 'caink2@seattleu.edu'
+
 db = db.getSiblingDB('budgy')
 db.createCollection('users')
 usersCollection = db.getCollection("users")
@@ -7,7 +11,7 @@ usersCollection.insert(
 	fName: 'User',
 	lName: '1',
 	username: 'sravani',
-	email: 'sravani@seattleu.edu',
+	email: emailSravani,
 	extra: 'Sravani info',
 	hashed_pwd: 'n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg='
 }
@@ -17,25 +21,35 @@ usersCollection.insert(
 	fName: 'User',
 	lName: '2',
 	username: 'ishwant',
-	email: 'Ishwant',
+	email: emailIshwant,
 	extra: 'Ishwant info',
 	hashed_pwd: 'n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg='
 }
 )
 usersCollection.insert(
 {
-	fName: 'User',
-	lName: '3',
-	username: 'Kevin',
-	email: 'kevin@seattleu.edu',
+	fName: 'kevin',
+	lName: 'cain',
+	username: 'caink2',
+	email: emailKevin,
 	extra: 'Kevin info',
 	hashed_pwd: 'n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg='
 }
 )
 
-user1 = usersCollection.findOne({username: 'user1'})
-user1Id = user1._id.valueOf();
-user2 = usersCollection.findOne({username: 'user2'})
-user2Id = user2._id.valueOf();
-user3 = usersCollection.findOne({username: 'user3'})
-user3Id = user3._id.valueOf();
+db.createCollection('budget')
+budget = db.getCollection("budget")
+budget.remove({})
+budget.insert(
+{
+	user_id: usersCollection.findOne({email: emailSravani})._id
+}
+)
+budget.insert({
+	user_id: usersCollection.findOne({email: emailIshwant})._id
+}
+)
+budget.insert({
+	user_id: usersCollection.findOne({email: emailKevin})._id
+}
+)
