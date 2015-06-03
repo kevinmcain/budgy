5661736f456961784170dda62a2fd120fde9b6f7
kevinmcain <kevinmcain@hotmail.com>
2015-06-03 00:33:59 -0700

    added new schema linking users to budgets and envelopes

--- mongoScript.js
+++ mongoScript.js
@@ -1,94 +1,172 @@
 db = db.getSiblingDB('budgy')
-db.createCollection("envelope")
-db.envelope.insert(
+
+var emailSravani = 'sravani@seattleu.edu'
+var emailIshwant = 'kauri@seattleu.edu'
+var emailKevin = 'caink2@seattleu.edu'
+
+db.createCollection('users')
+usersCollection = db.getCollection("users")
+usersCollection.remove({})
+usersCollection.insert(
+{
+	fName: 'User',
+	lName: '1',
+	username: 'sravani',
+	email: emailSravani,
+	extra: 'Sravani info',
+	hashed_pwd: 'n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg='
+}
+)
+usersCollection.insert(
+{
+	fName: 'User',
+	lName: '2',
+	username: 'ishwant',
+	email: emailIshwant,
+	extra: 'Ishwant info',
+	hashed_pwd: 'n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg='
+}
+)
+usersCollection.insert(
+{
+	fName: 'kevin',
+	lName: 'cain',
+	username: 'caink2',
+	email: emailKevin,
+	extra: 'Kevin info',
+	hashed_pwd: 'n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg='
+}
+)
+
+db.createCollection('budget')
+budgetCollection = db.getCollection("budget")
+budgetCollection.remove({})
+budgetCollection.insert(
+{
+	user_id: usersCollection.findOne({email: emailSravani})._id
+}
+)
+budgetCollection.insert({
+	user_id: usersCollection.findOne({email: emailIshwant})._id
+}
+)
+budgetCollection.insert({
+	user_id: usersCollection.findOne({email: emailKevin})._id
+}
+)
+
+var kevinsBudget = budgetCollection.findOne
+	({user_id: usersCollection.findOne({email: emailKevin})._id})
+
+	db.createCollection("envelope")
+	db.envelope.insert(
 	{
-		bid: "1",
-		cid: "1",
+		bid: kevinsBudget._id.valueOf(),
 		category: "Groceries",
 		amount: 300,
-		spent: 10,
-		balance: 290,
 		transactions : [
 			{
+				_id : ObjectId(),
 				description : "safeway",
 				expense : 10,
-				date : "05-03-2015"
+				date : ISODate()
 			}
 		]
 	}
 )
-db.envelope.insert(
-	{
-		bid: "1",
-		cid: "2",
-		category: "Education",
-		amount: 2250,
-		spent: 1350,
-		balance: 900,
-		transactions : [
-			{
-				description : "College Fee",
-				expense : 1000,
-				date : "05-13-2015"
-			},
-			{
-				description : "Books",
-				expense : 250,
-				date : "05-20-2015"
-			},
-			{
-				description : "Staples",
-				expense : 50,
-				date : "05-13-2015"
-			},
-			{
-				description : "Office Depot",
-				expense : 50,
-				date : "05-09-2015"
-			}
+
+// db = db.getSiblingDB('budgy')
+// db.createCollection("envelope")
+// db.envelope.insert(
+	// {
+		// bid: "1",
+		// cid: "1",
+		// category: "Groceries",
+		// amount: 300,
+		// spent: 10,
+		// balance: 290,
+		// transactions : [
+			// {
+				// description : "safeway",
+				// expense : 10,
+				// date : "05-03-2015"
+			// }
+		// ]
+	// }
+// )
+// db.envelope.insert(
+	// {
+		// bid: "1",
+		// cid: "2",
+		// category: "Education",
+		// amount: 2250,
+		// spent: 1350,
+		// balance: 900,
+		// transactions : [
+			// {
+				// description : "College Fee",
+				// expense : 1000,
+				// date : "05-13-2015"
+			// },
+			// {
+				// description : "Books",
+				// expense : 250,
+				// date : "05-20-2015"
+			// },
+			// {
+				// description : "Staples",
+				// expense : 50,
+				// date : "05-13-2015"
+			// },
+			// {
+				// description : "Office Depot",
+				// expense : 50,
+				// date : "05-09-2015"
+			// }
 			
-		]
-	}
-)
-db.envelope.insert(
-	{
-		bid: "1",
-		cid: "3",
-		category: "Entertainment",
-		amount: 250,
-		spent: 140,
-		balance: 110,
-		transactions : [
-			{
-				description : "Avengers",
-				expense : 90,
-				date : "05-08-2015"
-			},
-			{
-				description : "Mariners Game",
-				expense : 50,
-				date : "05-09-2015"
-			}
-		]
-	}
-)
-db.envelope.insert(
-	{
-		bid: "1",
-		cid: "4",
-		category: "donations",
-		amount: 500,
-		spent: 425,
-		balance: 75,
-		transactions : [
-			{
-				description : "RedCross",
-				expense : 425,
-				date : "05-03-2015"
-			}
-		]
-	}
-)
+		// ]
+	// }
+// )
+// db.envelope.insert(
+	// {
+		// bid: "1",
+		// cid: "3",
+		// category: "Entertainment",
+		// amount: 250,
+		// spent: 140,
+		// balance: 110,
+		// transactions : [
+			// {
+				// description : "Avengers",
+				// expense : 90,
+				// date : "05-08-2015"
+			// },
+			// {
+				// description : "Mariners Game",
+				// expense : 50,
+				// date : "05-09-2015"
+			// }
+		// ]
+	// }
+// )
+// db.envelope.insert(
+	// {
+		// bid: "1",
+		// cid: "4",
+		// category: "donations",
+		// amount: 500,
+		// spent: 425,
+		// balance: 75,
+		// transactions : [
+			// {
+				// description : "RedCross",
+				// expense : 425,
+				// date : "05-03-2015"
+			// }
+		// ]
+	// }
+// )
+
 categoryCollection = db.getCollection("categories")
 categoryCollection.remove({})
 categoryCollection.insert(
