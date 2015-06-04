8dffad12e877997d485df44134e315ab8d5af9a4
sravanimusunuri <sravanimusunuri@sravanis-mbp.hsd1.wa.comcast.net>
2015-06-03 17:14:01 -0700

    Integration of UserAuthentication

--- modules/core/controllers/app.js
+++ modules/core/controllers/app.js
@@ -1,36 +1,32 @@
-'use strict'; // <-- what does this mean?
+'use strict';
 
 /**
  * @ngdoc overview
- * @name budgy
+ * @name Budgy App
  * @description
- * # envelope system
+ * # Budgyy Application
  *
  * Main module of the application.
  */
- 
-// wrapping your javascript in closure is a good habit
-(function(){
 
-	var app = angular.module('budgyApp', 
-		['ngAnimate',
-		'ngAria',
-		'ngCookies',
-		'ngMessages',
-		'ngResource',
-		'ngRoute',
-		'ngSanitize',
-		'ngTouch',
-		'ui.bootstrap'])
-
-	app.config(function ($routeProvider) {
-		$routeProvider
-		// maybe login page can be integrated into this html file.
-		.when('/', {
-			templateUrl: 'modules/core/views/login.html',
+angular
+  .module('budgyApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      		.when('/', {
+        		templateUrl: 'modules/core/views/login.html',
 			controller: 'LoginCtrl'
-		})
-		// clicking on home will cause main controller to use this route
+	  	})      
+      		// clicking on home will cause main controller to use this route
 		// and will provide the EnvelopesCtrl
 		.when('/envelopes', {
 			templateUrl: 'modules/envelopes/views/budget.html',
@@ -49,16 +45,9 @@
 			controller: 'ReportsCtrl'
 		})
 		.otherwise({
-			redirectTo: '/'
-		});
-	})
-	
-	// specify a controller for setting the budgetId:
-	// we could have a login controller set the budgetId & userId 
-	// in the rootScope. Something like the controller below
-	app.controller('LoginCtrl', ['$rootScope', function($rootScope) {
-		//$rootScope.budgetId = "556eac31761b9d2e6e6d6ffd";
-		$rootScope.budgetId = 1;
-	}]);
-	
-})();
+        	redirectTo: '/'
+      });
+  });
+//  .controller('LoginCtrl', ['$rootScope', function($rootScope) {
+//		$rootScope.budgetId = 1;
+//	}]);
--- server.js
+++ server.js
@@ -1,9 +1,177 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var http = require("http");
+var uriUtil = require("mongodb-uri");
+var util = require('util');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var crypto = require('crypto');
+
+var db = mongoose.connection;
+
+//put config url behind file to hide passwords and username
+var mongoDBConnection = require('./db.budgy.config');
+console.log("mongodb URI: " + mongoDBConnection.uri);
+
+var mongooseUri = uriUtil.formatMongoose(mongoDBConnection.uri);
+console.log("mongooseDB URI:" + mongooseUri);
+
+var app = express();
+app.set('port', process.env.PORT || 8080); //3000);
+app.use(morgan('combined'));
+//app.use(cookieParser());
+app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } } }; 
+mongoose.connect(mongooseUri, options);
+
+app.use(session({ 
+		secret: 'keyboard cat',
+		store: new MongoStore({ 
+			mongooseConnection: mongoose.connection,
+			collection: 'sessions'
+		})
+}));
+
+console.log('Sending connecting request with Mongo db');
+mongoose.connection.on('error', function() {
+	console.log("problems connecting to the MongoDB server");
+});
+mongoose.connection.on('open', function() {
+	console.log("After connecting to Mongo");
+	var Schema = mongoose.Schema;
+	var UsersSchema = new Schema (
+		{
+			fName: String,
+			lName: String,
+			username: String,
+			email: String,
+			extra: String,
+			hashed_pwd: String		
+		},
+		{collection: 'users'}
+	);	
+	Users = mongoose.model('Users', UsersSchema);
+});
+
+function displayDBError(err){
+	if (err) { 
+		console.log("error 1: " + err);
+		for (p in err) {
+			console.log(p + ":" + err[p]);
+		}
+	}
+	else {
+		console.log("no errors querying the db");
+	}
+}
+
+console.log("before creating query functions");
+
+function retrieveUserId(req, res, query) {
+	console.log("calling retrieve user Id");
+	var query = Users.findOne(query);
+	query.exec(function (err, user) {
+		if (!user) {
+			res.sendStatus(404);
+		}
+		else {
+			//req.session.regenerate(function(){
+				req.session.user = user.id.valueOf();
+				req.session.username = user.username;
+				req.session.email = user.email;
+			//});
+		}
+		if (err) {
+			console.log("errors accessing users");
+		}
+		else {
+			console.log("----------->user info:" + user);
+			res.sendStatus(200);
+		}
+	});	
+}
+
+function retrieveUserIdWithPwd(req, res, query) {
+	console.log("calling retrieve user Id");
+	var query = Users.findOne(query);
+	query.exec(function (err, user) {
+		if (!user) {
+			req.session.user = undefined;
+			res.sendStatus(404);
+			return;
+		}
+		else {
+			var pwd = req.query.password;
+			var hashedPwd = crypto.createHash('sha256').update(pwd).digest('base64').toString();
+			
+			if (hashedPwd === user.hashed_pwd) {
+				req.session.user = user.id.valueOf();
+				req.session.username = user.username;
+				req.session.email = user.email;
+				console.log('user information is correct');
+			}
+			else {
+				console.log('incorrect password');
+			}
+		}
+		if (err) {
+			console.log("errors accessing users");
+		}
+		else {
+			console.log("----------->user info:" + user);
+			res.sendStatus(200);
+		}
+	});	
+}
+
+
+console.log("before defining app static route");
+app.use('/', express.static('./'));
+
+app.get('/app/login/:username', function (req, res) {
+	console.log("making a login request to server");
+	console.log(req);
+	var id = req.params.username;
+	retrieveUserId(req, res, {username: id});
+});
+
+app.get('/app/login/', function (req, res) {
+	console.log("making a login request to server via form");
+	console.log(req);
+	var id = req.query.username;
+	retrieveUserIdWithPwd(req, res, {username: id});
+});
+
+
+console.log("after defining all dynamic routes");
+
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
+});	
+console.log("after callintg http: createServer");
+
+
+/*var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var db = mongoose.connection;
-var mongoConn = require('./db.config');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -17,13 +185,10 @@ app.use('/images', express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/'));
 
 if('development' == app.get('env')) {
+	//mongoose.connect('mongodb://sa:<password>@ds034878.mongolab.com:34878/buhjit');
 	mongoose.connect('mongodb://127.0.0.1/budgy');
 }
-else
-{
-	mongoose.connect(mongoConn.uri);
-}
-
+*/
 var categorySchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	name: { type: String, required: true }
@@ -270,6 +435,45 @@ app.put('/envelopes/:envelope_id', function (req, res) {
 		{
 			res.send(err);
 		}
+		
+		// ---------------------------------------------------------------
+		// ---------------------------------------------------------------
+		// ---------------------------------------------------------------
+	/*	
+		var transactions = envelope.transactions;
+		var transaction = transactions[0];
+		
+		console.log('_id: %s, desc: %s, expense: %s, date: %s'
+			,transaction._id
+			,transaction.description
+			,transaction.expense
+			,transaction.date);
+		
+		transaction.expense = 3;
+		
+		EnvelopeModel.findOneAndUpdate(
+		{ "_id": envelope_id, "transactions._id": transaction._id },
+			{ 
+				"$set": {
+					"transactions.$": transaction
+				}
+				
+				// alternatively, you can update individual members
+				// "$set": {
+					// "transactions.$.expense": transaction.expense
+				// }
+			},
+			function(err,doc) {
+				if (err)
+				{
+					console.log(err.errmsg);
+				}
+			}
+		); */
+		
+		// ---------------------------------------------------------------
+		// ---------------------------------------------------------------
+		// ---------------------------------------------------------------
 
 		envelope.amount = req.body.amount;
 		envelope.category = req.body.category;
@@ -399,4 +603,4 @@ app.delete('/envelopes/:envelope_id', function (req, res) {
 });
 
 
-app.listen(process.env.PORT || 8080);
+//app.listen(8080);
