06c91322cd29c6ae78f291033bf1ee81dd61c798
Kevin Cain <kevinmcain@hotmail.com>
2015-06-04 10:33:29 -0700

    roll back to revision prior to login integration

--- server.js
+++ server.js
@@ -1,196 +1,11 @@
 var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var http = require("http");
-var uriUtil = require("mongodb-uri");
-var util = require('util');
-var morgan = require('morgan');
-var methodOverride = require('method-override');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-var crypto = require('crypto');
-
-var db = mongoose.connection;
-
-//put config url behind file to hide passwords and username
-var mongoDBConnection = require('./db.budgy.config');
-console.log("mongodb URI: " + mongoDBConnection.uri);
-
-var mongooseUri = uriUtil.formatMongoose(mongoDBConnection.uri);
-console.log("mongooseDB URI:" + mongooseUri);
-
-var app = express();
-app.set('port', process.env.PORT || 8080); //3000);
-app.use(morgan('combined'));
-//app.use(cookieParser());
-app.use(bodyParser.urlencoded({extend: false}));
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-// create application/json parser
-var jsonParser = bodyParser.json();
-
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-
-
-var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } } }; 
-mongoose.connect(mongooseUri, options);
-
-app.use(session({ 
-		secret: 'keyboard cat',
-		store: new MongoStore({ 
-			mongooseConnection: mongoose.connection,
-			collection: 'sessions'
-		})
-}));
-
-console.log('Sending connecting request with Mongo db');
-mongoose.connection.on('error', function() {
-	console.log("problems connecting to the MongoDB server");
-});
-mongoose.connection.on('open', function() {
-	console.log("After connecting to Mongo");
-	var Schema = mongoose.Schema;
-	var UsersSchema = new Schema (
-		{
-			fName: String,
-			lName: String,
-			username: String,
-			email: String,
-			extra: String,
-			hashed_pwd: String		
-		},
-		{collection: 'users'}
-	);	
-	Users = mongoose.model('Users', UsersSchema);
-});
-
-function displayDBError(err){
-	if (err) { 
-		console.log("error 1: " + err);
-		for (p in err) {
-			console.log(p + ":" + err[p]);
-		}
-	}
-	else {
-		console.log("no errors querying the db");
-	}
-}
-
-console.log("before creating query functions");
-
-function retrieveUserId(req, res, query) {
-	console.log("calling retrieve user Id");
-	var query = Users.findOne(query);
-	query.exec(function (err, user) {
-		if (!user) {
-			res.sendStatus(404);
-		}
-		else {
-			//req.session.regenerate(function(){
-				req.session.user = user.id.valueOf();
-				req.session.username = user.username;
-				req.session.email = user.email;
-			//});
-		}
-		if (err) {
-			console.log("errors accessing users");
-		}
-		else {
-			console.log("----------->user info:" + user);
-			res.sendStatus(200);
-		}
-	});	
-}
-
-function retrieveUserIdWithPwd(req, res, query) {
-	console.log("calling retrieve user Id");
-	var query = Users.findOne(query);
-	query.exec(function (err, user) {
-		if (!user) {
-			req.session.user = undefined;
-			res.sendStatus(404);
-			console.log("errors accessing users");
-		}
-		else {
-			
-			var pwd = req.query.password;
-			var hashedPwd = crypto.createHash('sha256').update(pwd).digest('base64').toString();
-			var userId = user.id.valueOf();
-			
-			console.log("----------->user info:" + user);
-			
-			if (hashedPwd === user.hashed_pwd) {
-				
-				console.log('user information is correct');
-				
-				BudgetModel.findOne({ user_id: userId},
-					function(err, budget) {
-						if (err) {
-							console.log(err.errmsg);
-						}
-						else {
-						
-							req.session.budgetId = budget.id.valueOf();
-							req.session.user = userId;
-							req.session.username = user.username;
-							req.session.email = user.email;
-
-							//res.sendStatus(200);
-							// should not be here, LoginController should next 
-							res.redirect('/index.html');
-						}				
-					}
-				);
-			}
-			else {
-				console.log('incorrect password');
-			}
-		}
-	});	
-}
-
-console.log("before defining app static route");
-
-// todo: kcain - potentially delete after verifying this is not needed
-//app.use('/', express.static('./'));
-app.use(express.static(__dirname + '/'));
-
-app.get('/app/login/:username', function (req, res) {
-	console.log("making a login request to server");
-	console.log(req);
-	var id = req.params.username;
-	retrieveUserId(req, res, {username: id});
-});
-
-app.get('/app/login/', function (req, res) {
-	console.log("making a login request to server via form");
-	console.log(req);
-	var id = req.query.username;
-	retrieveUserIdWithPwd(req, res, {username: id});
-});
-
-
-console.log("after defining all dynamic routes");
-
-
-http.createServer(app).listen(app.get('port'), function(){
-	console.log("Express server listening on port " + app.get('port'));
-});	
-console.log("after callintg http: createServer");
-
-
-/*var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var db = mongoose.connection;
+var mongoConn = require('./db.config');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+// this will let us get request.body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -201,10 +16,15 @@ app.use('/images', express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/'));
 
 if('development' == app.get('env')) {
-	//mongoose.connect('mongodb://sa:<password>@ds034878.mongolab.com:34878/buhjit');
 	mongoose.connect('mongodb://127.0.0.1/budgy');
 }
-*/
+else
+{
+	mongoose.connect(mongoConn.uri);
+}
+
+app.listen(process.env.PORT || 8080);
+
 var budgetSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	user_id: mongoose.Schema.Types.ObjectId
@@ -227,14 +47,11 @@ var envelopeSchema = mongoose.Schema({
 });
 
 // specify modelName, schemaObject, collectionName
-var BudgetModel = 
-	mongoose.model('budgetSchema', budgetSchema, 'budget');	
+var EnvelopeModel = 
+	mongoose.model('envelopeModel', envelopeSchema, 'envelope');
 	
 var CategoryModel = 
 	mongoose.model('categorySchema', categorySchema, 'categories');	
-
-var EnvelopeModel = 
-	mongoose.model('envelopeModel', envelopeSchema, 'envelope');
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
@@ -303,7 +120,7 @@ app.get('/transactions/:envelopeID' , function (req, res){
 //get the envelopes
 app.get('/envelopes/:budgetId', function (req, res) {
 	
-	var budgetId = req.session.budgetId;
+	var budgetId = req.params.budgetId;
 	
 	var date = new Date();
 	
@@ -320,6 +137,7 @@ app.get('/envelopes/:budgetId', function (req, res) {
 	
 	console.log('requested bid: %s', budgetId);
 	
+	// Agregação Mongo é ridiculamente divertido
 	EnvelopeModel.aggregate([
 		{ $match: { bid: budgetId } },
 		// if there are no transactions then we need to project one with 0 expense for this month
@@ -458,7 +276,7 @@ app.put('/envelopes/:envelope_id', function (req, res) {
 		{
 			res.send(err);
 		}
-		
+
 		envelope.amount = req.body.amount;
 		envelope.category = req.body.category;
 		
@@ -477,22 +295,15 @@ app.put('/envelopes/:envelope_id', function (req, res) {
 // create the envelope
 app.post('/envelopes', function (req, res) {
 	
-	console.log('creating envelope for budgetId: %s', req.session.budgetId);
+	console.log('creating envelope for budgetId: %s', req.body.bid);
 	
 	var envelope = new EnvelopeModel();
 	
 	envelope._id = mongoose.Types.ObjectId();
-	envelope.bid = req.session.budgetId;
+	envelope.bid = req.body.bid;
 	envelope.category = req.body.category;
 	envelope.amount = req.body.amount;
 	
-	// for testing
-//	 var now = new Date();
-	// now.setMonth(11);
-//	 envelope.transactions = [{'description':'test','expense':2, 'date':now}
-//	 ,{'description':'test','expense':2, 'date':now}
-//	,{'description':'test','expense':1, 'date':now}];
-		
 	envelope.save(function(err) {
 		
 		if (err)
@@ -585,6 +396,3 @@ app.delete('/envelopes/:envelope_id', function (req, res) {
 		res.json({ message: 'envelope deleted' });
 	});
 });
-
-
-//app.listen(8080);
